594ca87d2e78c7cf8b35bc3fcecaa364eede428e
shiv0403 <shiv0403gupta@gmail.com>
2022-05-14 02:02:28 +0530

    request details

--- src/components/molecules/AboutAd/AboutSeller.jsx
+++ src/components/molecules/AboutAd/AboutSeller.jsx
@@ -1,13 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomButton from "../../atoms/CustomButton/CustomButton";
 import CallOutlinedIcon from "@material-ui/icons/CallOutlined";
 import ReportOutlinedIcon from "@material-ui/icons/ReportOutlined";
+import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
 import Avatar from "@material-ui/core/Avatar";
 import moment from "moment";
 import Flower from "../../../assets/sample.jpeg";
+import axios from "../../../utils/axios";
+import { errorToast, successToast } from "../../../utils/toast";
 
 function AboutSeller({ ad }) {
+  const userId = localStorage.getItem("userId");
   const [user, setUser] = useState(ad?.user);
+  const [status, setStatus] = useState(0);
+
+  const handleRequestDetails = async () => {
+    await axios
+      .put("/ad-request", {
+        buyerId: userId,
+        sellerId: user.id,
+        type: "request",
+        adId: ad.id,
+        status: 2, //pending
+      })
+      .then((response) => {
+        successToast("Request sent", 3000);
+        setStatus(2);
+      })
+      .catch((err) => {
+        errorToast(err.message);
+      });
+  };
+
+  useEffect(() => {
+    console.log("ad details", ad);
+    async function getStatus() {
+      await axios
+        .get("/ad-request-status", {
+          params: {
+            buyerId: userId,
+            sellerId: user.id,
+          },
+        })
+        .then((response) => {
+          setStatus(response.data.status);
+        })
+        .catch((err) => {
+          console.log(err);
+          errorToast(err.message);
+        });
+    }
+
+    getStatus();
+  }, []);
 
   return (
     <div
@@ -37,12 +84,45 @@ function AboutSeller({ ad }) {
         </div>
       </div>
 
-      <div className="mt-4 mb-2">
-        <CustomButton
-          text="Requst for details"
-          className="px-5 py-3 tracking-widest text-20 w-full"
-        />
-      </div>
+      {status === 1 ? (
+        <div className="ml-2 mt-3">
+          {/* phone */}
+          <div className="flex items-center">
+            <div className="mr-5">
+              <PhoneIcon />
+            </div>
+            <div>
+              <p className="font-bold tracking-widest text-lg">{user?.phone}</p>
+            </div>
+          </div>
+
+          {/* email */}
+          <div className="flex items-center mt-2">
+            <div className="mr-5">
+              <EmailIcon />
+            </div>
+            <div>
+              <p className="font-bold tracking-widest text-lg">{user?.email}</p>
+            </div>
+          </div>
+        </div>
+      ) : status === 2 ? (
+        <div>
+          <div className="mt-4 mb-2">
+            <p className="px-5 py-3 tracking-widest text-20 w-full border-2 border-primary text-center bg-offWhite">
+              Request pending
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-4 mb-2">
+          <CustomButton
+            text="Requst for details"
+            className="px-5 py-3 tracking-widest text-20 w-full"
+            onClick={handleRequestDetails}
+          />
+        </div>
+      )}
     </div>
   );
 }
