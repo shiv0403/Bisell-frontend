e68c99375a8798ff10dfbbd872135ad1c556d897
shiv0403 <shiv0403gupta@gmail.com>
2022-07-09 03:42:17 +0530

    axios settings

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,4 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://bisell-backend.herokuapp.com/v1",
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
