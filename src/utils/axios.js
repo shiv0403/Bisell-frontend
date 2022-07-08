e660c9bfc8a556e0bd53a184ce4ceb6b80e2eb00
shiv0403 <shiv0403gupta@gmail.com>
2022-07-09 03:35:50 +0530

    base url change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/v1",
+  baseURL: "https://bisell-backend.herokuapp.com/v1",
   withCredentials: true,
 });
